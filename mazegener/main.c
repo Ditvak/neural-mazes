b2607de9d57e61f20bb734917fb37ebed477232b
harrisondowns <harrisonmdowns@gmail.com>
2017-07-10 15:46:58 -0400

    Initial Commit

---
+++ mazegener/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  mazegener
+//
+//  Created by Groot Groot on 7/10/17.
+//  Copyright © 2017 Harrison Downs. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
