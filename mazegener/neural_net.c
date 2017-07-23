7b057c971f15a2adcd41390d211774c775c274de
harrisondowns <harrisonmdowns@gmail.com>
2017-07-23 16:13:31 -0400

    -Added neural_net.h and neural_net.c -Started to realize how hard this is going to be :(

---
+++ mazegener/neural_net.c
@@ -0,0 +1,9 @@
+//
+//  neural_net.c
+//  mazegener
+//
+//  Created by Groot Groot on 7/23/17.
+//  Copyright © 2017 Harrison Downs. All rights reserved.
+//
+
+#include "neural_net.h"
---
+++ mazegener/neural_net.h
@@ -0,0 +1,14 @@
+//
+//  neural_net.h
+//  mazegener
+//
+//  Created by Groot Groot on 7/23/17.
+//  Copyright © 2017 Harrison Downs. All rights reserved.
+//
+
+#ifndef neural_net_h
+#define neural_net_h
+
+#include <stdio.h>
+
+#endif /* neural_net_h */
